6d150c776d5130a4980986a9d15029ffe4bd4bbd
gcdamazio <damaziogabrielle@gmail.com>
2024-10-23 20:37:26 -0400

    finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,14 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+        Person amor = new Person("Bill", 34, 180);
+        Car car1 = new Car("Toyota","Corolla",2015,60000);
+        Chair chair1 = new Chair("wood","black",3);
+        Dog roxy =new Dog("Roxy",9,"labrador");
+        System.out.println(roxy.getName() + " "+ "is " + roxy.getAge());
+        roxy.setAge(11);
+        System.out.println(roxy.getName() + " "+ "will be " + roxy.getAge() + "yo in two years.");
+
+    }
 }
---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,26 @@
+package object1;
+
+// Define a class
+class Person {
+    // Define Instance Variables
+    // different for each object!
+    String name;
+    int age = 0;
+    int height = 0;
+
+    // Constructor
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height; //in centimeters
+    }
+
+    public static void main(String[] args) {
+        // Declare and initialize an object
+        Person person1 = new Person("John", 30, 170);
+        Person person2 = new Person("Bill", 34, 180);
+        System.out.println(person2);
+        /* Now you can use the Object `person1` */
+
+    }
+}
